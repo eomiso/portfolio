153352d25a4eaf9fe9ef1a560c090032291e2067
Uiseop Eom <ytrtef@gmail.com>
2022-03-12 03:30:21 +0900

    changed about and projects

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <h3>Hi I'm a developer who often leaps through stuff</h3>
+      <h4> Seoul Nat'l University 2022, CS & Linguistics </h4>
     </>
   );
 };
