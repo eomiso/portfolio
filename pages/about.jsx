5685b39b935f4f7d37c7d1205e86ae80810a44e5
Uiseop Eom <ytrtef@gmail.com>
2022-03-12 13:23:16 +0900

    Updated About page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,13 @@
+import styles from '../styles/AboutPage.module.css';
+
 const AboutPage = () => {
   return (
     <>
-      <h3>Hi I'm a developer who often leaps through stuff</h3>
-      <h4> Seoul Nat'l University 2022, CS & Linguistics </h4>
+      <h1> About me... </h1>
+      <div className={styles.headerMargin}></div>
+      <h3>컴퓨터와 관련된 거라면 무엇이든 눈길이 가는 <s>곁눈질은 이제 그만</s> 개발자 입니다.</h3>
+      <div className={styles.footerMargin}></div>
+      <h4> Seoul Nat'l University, CS & Linguistics class of 2022</h4>
     </>
   );
 };
